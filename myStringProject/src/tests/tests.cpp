6af029695c0ac75ed9f1fe31b0547b08e3694aca
DmitriyFilenko <filenko.dim911@gmail.com>
2024-02-22 08:47:03 +0300

    Добавлена папка myString с практическим заданием

---
+++ myStringProject/src/tests/tests.cpp
@@ -0,0 +1,20 @@
+
+#include <stdexcept>
+#include <gtest/gtest.h>
+#include "../funcs/funcs.hpp"
+
+
+TEST(testCase, testName) 
+{
+    EXPECT_EQ(1, 1);
+}
+
+TEST(testCase, testName) 
+{
+    EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
