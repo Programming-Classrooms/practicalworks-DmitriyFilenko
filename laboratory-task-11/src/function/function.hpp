ef81f384a0af3fecace18e23220ab0f6894de91d
DmitriyFilenko <filenko.dim911@gmail.com>
2024-04-07 18:45:40 +0300

    Исправлена ошибка названия файла

---
+++ laboratory-task-11/src/function/function.hpp
@@ -0,0 +1,36 @@
+
+#ifndef FUNCS_HPP
+#define FUNCS_HPP
+
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include <stdexcept>
+
+
+struct Employee {
+  std::string surname;
+  size_t children;
+};
+
+
+bool IsEven(int);
+
+bool CompareByParity(int, int);
+
+bool CompareEmployees(const Employee&, const Employee&);
+
+template <typename T>
+void Sort(T* , int , bool (*compare)(const T&, const T&));
+
+int* ReadNumbersFromFile(const std::string& , int* );
+
+std::string* ReadSurnamesFromFile(const std::string& , int* );
+
+Employee* CreateEmployees(std::string* , int* , int );
+
+void WriteEmployeesToFile(const std::string& , Employee* , int );
+
+void WriteEmployeesToFileBinary(const std::string& , Employee* , int );
+
+#endif //FUNCS_HPP
